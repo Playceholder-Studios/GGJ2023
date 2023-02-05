004673475415a496b664fdf96fa6d8b32dbcb243
Terry Wen <terrywen@terrys-mac-mini.lan>
2023-02-04 20:38:27 -0500

    Really silly indicators

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,6 +6,7 @@ public class LevelManager : MonoBehaviour
     public static LevelManager Instance { get; private set; }
 
     public List<int> currentIngredients;
+    public List<GameObject> ingredientSlots;
     public bool iceAdded;
 
     public int currentDrink;
@@ -13,6 +14,9 @@ public class LevelManager : MonoBehaviour
     public Level currentLevel;
 
     public GameObject activeIce;
+    public GameObject emptyCup;
+    public GameObject successDrink;
+    public GameObject failureDrink;
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -72,9 +76,18 @@ public class LevelManager : MonoBehaviour
     public void ClearIngredients()
     {
         currentIngredients = new List<int>();
+
+        foreach (GameObject slot in ingredientSlots)
+        {
+            SetSlot(slot, -1);
+        }
         iceAdded = false;
         activeIce.SetActive(false);
         currentDrink = -1;
+
+        emptyCup.SetActive(true);
+        successDrink.SetActive(false);
+        failureDrink.SetActive(false);
     }
 
     public bool AddIngredient(int id)
@@ -82,6 +95,7 @@ public class LevelManager : MonoBehaviour
         if (currentIngredients.Count < 3 && !currentIngredients.Contains(id))
         {
             Debug.Log("Added ingredient " + id);
+            SetSlot(ingredientSlots[currentIngredients.Count], id);
             currentIngredients.Add(id);
             return true;
         }
@@ -97,21 +111,38 @@ public class LevelManager : MonoBehaviour
             Debug.Log("Drink already mixed");
             return;
         }
+        emptyCup.SetActive(false);
         AudioManager.Instance.PlayEffect("MIX");
         foreach (Recipe r in GameManager.Instance.Recipes.Values) {
             bool isMatch = r.TryRecipe(currentIngredients, iceAdded);
             Debug.Log("Trying Recipe " + r.recipeName + ": " + isMatch);
             if (isMatch)
             {
+                successDrink.SetActive(true);
                 currentDrink = r.id;
                 Debug.Log("Recipe found! " + r.id);
                 return;
             }
         }
 
+        failureDrink.SetActive(true);
         currentDrink = 0;
         Debug.Log("Not a recipe!");
         return;
+    }
+
+    private void SetSlot(GameObject ingSlot, int ingredientId) {
+        foreach(Transform child in ingSlot.transform)
+        {
+            if (child.gameObject.name == ingredientId.ToString())
+            {
+                child.gameObject.SetActive(true);
+            }
+            else 
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
     }
 
     public void PlayDialogue(List<Dialogue> dialogue)
